5824d1554459af0bd9194aeafe9b166519bfa8b0
raj <raj@gmail.com>
2022-10-02 01:25:28 +0600

    All project done

---
+++ assets/js/app.js
@@ -0,0 +1,55 @@
+
+
+const marag_check = document.getElementById('marag_check');
+const msg = document.querySelector('.msg');
+const msgL = document.querySelector('.msg-long');
+const msg_dis = document.querySelector('#msg_dis');
+
+
+
+
+
+marag_check.onsubmit = (e) => {
+    e.preventDefault();
+
+//get all input value
+
+    let name = marag_check.querySelector('input[name = "name"]');
+    let age = marag_check.querySelector('input[name = "age"]');
+    let gender = marag_check.querySelectorAll('input[name = "gender"]');
+
+
+ //get gender value
+
+    let genderName = '';
+    gender.forEach ( item => {
+        if (item.checked) {
+            genderName = item.value;
+        }
+    })
+
+
+ //Form validation
+
+
+    if ( name.value == '' || age.value == '' ) {
+        msg.innerHTML = setAlert('Fild can\'t be Empty');
+    }else if ( genderName == '' ){
+        msg.innerHTML = setAlert('Must select Gender', 'info');
+    }else if ( CheckNumber(age.value) == false ){
+        msg.innerHTML = setAlert('Invalid Age','info');
+    } else {
+        msg.innerHTML = '';
+
+        //check marrage age
+        if ( genderName == 'male'){
+            msg_dis.style.display = 'block';
+            marrageAgeCal(name.value, age.value, genderName);
+        }else{
+            msg_dis.style.display = 'block';
+            marrageAgeCal(name.value, age.value, genderName);
+        }
+    }
+
+}
+
---
+++ assets/js/function.js
@@ -0,0 +1,59 @@
+
+/**
+ * Custom Alert Function 
+ */
+
+const setAlert = (msg, type = 'danger') => {
+    return `<p class="alert alert-${type} d-flex justify-content-between" role="alert">${msg} <button class="btn-close" data-bs-dismiss="alert"></button></p>`;
+}
+
+
+
+/**
+ * Number Check Function
+ */
+
+
+const CheckNumber = (num) => {
+    let numberPattern = /^[0-9]{1,3}$/;
+
+    return numberPattern.test(num);
+}
+
+
+
+
+/**
+ * Marrage age Check Function
+ */
+
+
+
+const marrageAgeCal = (name, age, gender) => {
+
+    //set marrage age
+    let marrageAge;
+    if ( gender == 'male' ){
+        marrageAge = 21;
+    }else{
+        marrageAge = 18;
+    }
+
+    //for make daynamic (vaiya/apu)
+    let genName = '';
+    if (gender == 'male'){
+        genName = 'vaiya';
+    }else{
+        genName = 'apuuu';
+    }
+
+
+    //Start calculation
+
+        if ( age >= marrageAge ){
+            return msgL.innerHTML = setAlert(`Hi, ${name} ${genName} you are readhy for Marrage.`,'success');
+        }else{
+            return msgL.innerHTML = setAlert(`Hi, ${name} ${genName} you are not readhy for Marrage, You have to wait ${marrageAge - age} years.`);
+
+    }
+}
